f538404f53468892ab391e081a85d5ab629df8c3
hoangduc <hoangduc.uit.dev@gmail.com>
2023-12-11 05:26:07 -0800

    Initial Commit

---
+++ thecoffeeshop/thecoffeeshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  thecoffeeshopApp.swift
+//  thecoffeeshop
+//
+//  Created by mac on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct thecoffeeshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
